6ade2509aa7a5c0dbc6849f771ea07b4323c690a
BaekGeon Kim <ooi0929.kim@gmail.com>
2024-09-02 11:38:32 +0900

    잘못된 파일 수정

--- flutter_widget/lib/main copy.dart
+++ flutter_widget/lib/main copy.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_widget/page/container_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: ContainerPage(),
     );
   }
 }
