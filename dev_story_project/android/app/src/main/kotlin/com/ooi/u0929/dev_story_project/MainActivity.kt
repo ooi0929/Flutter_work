6ee579b1b235570c516af92879e12ea9ffd30e83
BaekGeon Kim <ooi0929.kim@gmail.com>
2024-09-02 11:22:47 +0900

    Flutter 위젯 연습용 + DevStory 프로젝트 공부용

---
+++ dev_story_project/android/app/src/main/kotlin/com/ooi/u0929/dev_story_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ooi.u0929.dev_story_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_widget/android/app/src/main/kotlin/com/ooi/u0929/flutter_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ooi.u0929.flutter_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
