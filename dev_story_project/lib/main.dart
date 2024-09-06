e2081f0691c4a20257fe1a662251d72bab7717a6
BaekGeon Kim <ooi0929.kim@gmail.com>
2024-09-06 17:26:53 +0900

    수정전

--- dev_story_project/lib/main.dart
+++ dev_story_project/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:dev_story_project/textbook/ch01/main.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+void main() {
   runApp(const MyApp());
 }
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Container(),
     );
   }
 }
