4818821dc717d5455153de3a9e776b69b71e8460
ooi0929 <ooi0929.kim@gmail.com>
2024-10-09 12:46:12 +0900

    [Add] 프로젝트 Hello Flutter

--- lib/main_hello_flutter.dart
+++ lib/main_hello_flutter.dart
@@ -3,7 +3,7 @@
  * flutter build apk -t lib/main.dart
  */
 
-import 'package:flutter_workspace/core/environment.dart';
+import 'core/environment.dart';
 
 Future<void> main() async =>
     await Environment.newInstance(BuildType.hello).run();
